b31e139a2b40ed0814ee4f3cf331839a5e5adbd3
DavidHorvitz <2205dzh@gmail.com>
2021-12-01 10:20:44 +0200

    1

---
+++ Home_d_s.js
@@ -0,0 +1,542 @@
+
+    // alert('eeeeeeeeeeeeeeee');
+    let counter__a = 0;
+    let counter__b = 0;
+    let counter__c = 0;
+    let counter__d = 0;
+    let counter__e = 0;
+    let counter__f = 0;
+    let counter__g = 0;
+    let counter__h = 0;
+    let counter__i = 0;
+    let counter__j = 0;
+    let counter__k = 0;
+    let counter__l = 0;
+    let counter__m = 0;
+    let counter__n = 0;
+    let counter__o = 0;
+    let counter__p = 0;
+    let counter__q = 0;
+    let counter__r = 0;
+    let counter__s = 0;
+    let counter__t = 0;
+    let counter__u = 0;
+    let counter__v = 0;
+    let counter__w = 0;
+    let counter__x = 0;
+    let counter__y = 0;
+    let counter__z = 0;
+    let counter__aa = 0;
+    let counter__ab = 0;
+    let counter__ac = 0;
+    let counter__ad = 0;
+    let counter__ae = 0;
+    let counter__af = 0;
+    let counter__ag = 0;
+    let counter__ah = 0;
+    let counter__ai = 0;
+    let counter__aj = 0;
+    let all_counters = 0;
+
+    //1 line
+    function counter_a(){
+      
+      let a_id =  document.getElementById("a");
+      let x = document.getElementById("ae");
+      counter__a++;
+      all_counters++;
+     a_id.textContent = `אנדוקרינולוגיה  = ${counter__a}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_b(){
+      
+      let b_id =  document.getElementById("b");
+      let x = document.getElementById("ae");
+      counter__b++;
+      all_counters++;
+     b_id.textContent = `אלרגולוגיה  = ${counter__b}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_c(){
+      
+      let c_id =  document.getElementById("c");
+      let x = document.getElementById("ae");
+      counter__c++;
+      all_counters++;
+     c_id.textContent = `אורטופדיה  = ${counter__c}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_d(){
+      
+      let d_id =  document.getElementById("d");
+      let x = document.getElementById("ae");
+      counter__d++;
+      all_counters++;
+     d_id.textContent = `אורולוגיה  = ${counter__d}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_e(){
+      
+      let e_id =  document.getElementById("e");
+      let x = document.getElementById("ae");
+      counter__e++;
+      all_counters++;
+     e_id.textContent = `אונקולוגיה  = ${counter__e}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+     
+    }
+    function counter_f(){
+      
+      let f_id =  document.getElementById("f");
+      let x = document.getElementById("ae");
+      counter__f++;
+      all_counters++;
+     f_id.textContent = `א.א.ג  = ${counter__f}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+
+
+
+    // 2 line
+    function counter_g(){
+      
+      let g_id =  document.getElementById("g");
+      let x = document.getElementById("ae");
+      counter__g++;
+      all_counters++;
+     g_id.textContent = `טיפול נמרץ  = ${counter__g}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_h(){
+      
+      let h_id =  document.getElementById("h");
+      let x = document.getElementById("ae");
+      counter__h++;
+      all_counters++;
+     h_id.textContent = `המטולוגיה  = ${counter__h}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_i(){
+      
+      let i_id =  document.getElementById("i");
+      let x = document.getElementById("ae");
+      counter__i++;
+      all_counters++;
+     i_id.textContent = `גריאטרה  = ${counter__i}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_j(){
+      
+      let j_id =  document.getElementById("j");
+      let x = document.getElementById("ae");
+      counter__j++;
+      all_counters++;
+     j_id.textContent = `גסטרו  = ${counter__j}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_k(){
+      
+      let k_id =  document.getElementById("k");
+      let x = document.getElementById("ae");
+      counter__k++;
+      all_counters++;
+     k_id.textContent = `גניקולוגיה  = ${counter__k}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_l(){
+      
+      let l_id =  document.getElementById("l");
+      let x = document.getElementById("ae");
+      counter__l++;
+      all_counters++;
+     l_id.textContent = `גנטיקה  = ${counter__l}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+
+
+
+    //3 line
+    function counter_m(){
+      
+      let m_id =  document.getElementById("m");
+      let x = document.getElementById("ae");
+      counter__m++;
+      all_counters++;
+     m_id.textContent = `נירוכירורגיה  = ${counter__m}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_n(){
+      
+      let n_id =  document.getElementById("n");
+      let x = document.getElementById("ae");
+      counter__n++;
+      all_counters++;
+     n_id.textContent = `נירולוגיה  = ${counter__n}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_o(){
+      
+      let o_id =  document.getElementById("o");
+      let x = document.getElementById("ae");
+      counter__o++;
+      all_counters++;
+     o_id.textContent = `כללי  = ${counter__o}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_p(){
+      
+      let p_id =  document.getElementById("p");
+      let x = document.getElementById("ae");
+      counter__p++;
+      all_counters++;
+     p_id.textContent = `כירורגית ילדים  = ${counter__p}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_q(){
+      
+      let q_id =  document.getElementById("q");
+      let x = document.getElementById("ae");
+      counter__q++;
+      all_counters++;
+     q_id.textContent = `כירורגיה  = ${counter__q}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_r(){
+      
+      let r_id =  document.getElementById("r");
+      let x = document.getElementById("ae");
+      counter__r++;
+      all_counters++;
+     r_id.textContent = `יילודים  = ${counter__r}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+
+
+
+    // 4 line
+    function counter_s(){
+      
+      let s_id =  document.getElementById("s");
+      let x = document.getElementById("ae");
+      counter__s++;
+      all_counters++;
+     s_id.textContent = `פלסטיקה  = ${counter__s}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_t(){
+      
+      let t_id =  document.getElementById("t");
+      let x = document.getElementById("ae");
+      counter__t++;
+      all_counters++;
+     t_id.textContent = `פה ולסת  = ${counter__t}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_u(){
+      
+      let u_id =  document.getElementById("u");
+      let x = document.getElementById("ae");
+      counter__u++;
+      all_counters++;
+     u_id.textContent = `עיניים  = ${counter__u}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_v(){
+      
+      let v_id =  document.getElementById("v");
+      let x = document.getElementById("ae");
+      counter__v++;
+      all_counters++;
+     v_id.textContent = `עור  = ${counter__v}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_w(){
+      
+      let w_id =  document.getElementById("w");
+      let x = document.getElementById("ae");
+      counter__w++;
+      all_counters++;
+     w_id.textContent = `נשים  = ${counter__w}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_x(){
+      
+      let x_id =  document.getElementById("x");
+      let x = document.getElementById("ae");
+      counter__x++;
+      all_counters++;
+     x_id.textContent = `נפרולוגיה  = ${counter__x}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+
+
+
+    //5 line
+    // function counter_y(){
+      
+    //   let y_id =  document.getElementById("y");
+    //   counter__y++;
+    //  y_id.textContent = `ה  = ${counter__y}`;
+    // }
+    function counter_z(){
+      
+      let z_id =  document.getElementById("z");
+      let x = document.getElementById("ae");
+      counter__z++;
+      all_counters++;
+     z_id.textContent = `?  = ${counter__z}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_aa(){
+      
+      let aa_id =  document.getElementById("aa");
+      let x = document.getElementById("ae");
+      counter__aa++;
+      all_counters++;
+     aa_id.textContent = `ריאות  = ${counter__aa}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_ab(){
+      
+      let ab_id =  document.getElementById("ab");
+      let x = document.getElementById("ae");
+      counter__ab++;
+      all_counters++;
+     ab_id.textContent = `ראמטולוגיה  = ${counter__ab}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+     
+    }
+    function counter_ac(){
+      
+      let ac_id =  document.getElementById("ac");
+      let x = document.getElementById("ae");
+      counter__ac++;
+      all_counters++;
+     ac_id.textContent = `קרדיאולוגיה  = ${counter__ac}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+    function counter_ad(){
+      
+      let ad_id =  document.getElementById("ad");
+      let x = document.getElementById("ae");
+      counter__ad++;
+      all_counters++;
+     ad_id.textContent = `פנימית  = ${counter__ad}`;
+     x.textContent = `סה"כ = ${all_counters}`;
+    }
+
+
+    // 6 line
+    
+    // function counter_ae(){
+      
+      // let ae_id =  document.getElementById("ae");
+          
+      // ae_id.textContent = `סה"כ = ${all_counters}`;
+
+      
+ 
+    // }
+    function clear_all(){
+      
+      // let af_id =  document.getElementById("af");
+    
+    //  af_id.textContent = `אפס הכל `;
+     counter__a = 0;
+counter__b = 0;
+counter__c = 0;
+counter__d = 0;
+counter__e = 0;
+counter__f = 0;
+counter__g = 0;
+counter__h = 0;
+counter__i = 0;
+counter__j = 0;
+counter__k = 0;
+counter__l = 0;
+counter__m = 0;
+counter__n = 0;
+counter__o = 0;
+counter__p = 0;
+counter__q = 0;
+counter__r = 0;
+counter__s = 0;
+counter__t = 0;
+counter__u = 0;
+counter__v = 0;
+counter__w = 0;
+counter__x = 0;
+counter__y = 0;
+counter__z = 0;
+counter__aa = 0;
+counter__ab = 0;
+counter__ac = 0;
+counter__ad = 0;
+counter__ae = 0;
+// counter__af = 0;
+counter__ag = 0;
+counter__ah = 0;
+counter__ai = 0;
+counter__aj = 0;
+all_counters = 0;
+
+// 1 line
+let clear_a = document.getElementById("a");
+if(counter__a === 0){
+clear_a.textContent =  `אנדוקרינולוגיה  `;
+}
+let clear_b = document.getElementById("b");
+if(counter__b === 0){
+clear_b.textContent =  `אלרגולוגיה  `;
+}
+let clear_c = document.getElementById("c");
+if(counter__c === 0){
+clear_c.textContent =  `אורטופדיה  `;
+}
+let clear_d = document.getElementById("d");
+if(counter__d === 0){
+clear_d.textContent =  `אורולוגיה  `;
+}
+let clear_e = document.getElementById("e");
+if(counter__e === 0){
+clear_e.textContent =  `אונקולוגיה  `;
+}
+let clear_f = document.getElementById("f");
+if(counter__f === 0){
+clear_f.textContent =  `א.א.ג  `;
+}
+
+// 2 line
+let clear_g = document.getElementById("g");
+if(counter__g === 0){
+clear_g.textContent =  `טיפול נמרץ  `;
+}
+let clear_h = document.getElementById("h");
+if(counter__h === 0){
+clear_h.textContent =  `המטולוגיה  `;
+}
+let clear_i = document.getElementById("i");
+if(counter__i === 0){
+clear_i.textContent =  `גריאטרה  `;
+}
+let clear_j = document.getElementById("j");
+if(counter__j === 0){
+clear_j.textContent =  `גסטרו  `;
+}
+let clear_k = document.getElementById("k");
+if(counter__k === 0){
+clear_k.textContent =  `גניקולוגיה  `;
+}
+let clear_l = document.getElementById("l");
+if(counter__l === 0){
+clear_l.textContent =  `גנטיקה  `;
+}
+
+// 3 line
+let clear_m = document.getElementById("m");
+if(counter__m === 0){
+clear_m.textContent =  `נירוכירורגיה  `;
+}
+let clear_n = document.getElementById("n");
+if(counter__n === 0){
+clear_n.textContent =  `נירולוגיה  `;
+}
+let clear_o = document.getElementById("o");
+if(counter__o === 0){
+clear_o.textContent =  `כללי  `;
+}
+let clear_p = document.getElementById("p");
+if(counter__p === 0){
+clear_p.textContent =  `כירורכית ילדים  `;
+}
+let clear_q = document.getElementById("q");
+if(counter__q === 0){
+clear_q.textContent =  `כירורגיה  `;
+}
+let clear_r = document.getElementById("r");
+if(counter__r === 0){
+clear_r.textContent =  `יילודים  `;
+}
+
+// 4 line
+let clear_s = document.getElementById("s");
+if(counter__s === 0){
+clear_s.textContent =  `פלסטיקה  `;
+}
+let clear_t = document.getElementById("t");
+if(counter__t === 0){
+clear_t.textContent =  `פה ולסת  `;
+}
+let clear_u = document.getElementById("u");
+if(counter__u === 0){
+clear_u.textContent =  `עיניים  `;
+}
+let clear_v = document.getElementById("v");
+if(counter__v === 0){
+clear_v.textContent =  `עור  `;
+}
+let clear_w = document.getElementById("w");
+if(counter__w === 0){
+clear_w.textContent =  `נשים  `;
+}
+let clear_x = document.getElementById("x");
+if(counter__x === 0){
+clear_x.textContent =  `נפרולוגיה  `;
+}
+
+// 5 line
+// let clear_y = document.getElementById("y");
+// if(counter__y === 0){
+// clear_y.textContent =  `אנדוקרינולוגיה  `;
+// }
+let clear_z = document.getElementById("z");
+if(counter__z === 0){
+clear_z.textContent =  `?  `;
+}
+let clear_aa = document.getElementById("aa");
+if(counter__aa === 0){
+clear_aa.textContent =  `ריאות  `;
+}
+let clear_ab = document.getElementById("ab");
+if(counter__ab === 0){
+clear_ab.textContent =  `ראמטולוגיה  `;
+}
+let clear_ac = document.getElementById("ac");
+if(counter__ac === 0){
+clear_ac.textContent =  `קרדיאולוגיה  `;
+}
+let clear_ad = document.getElementById("ad");
+if(counter__ad === 0){
+clear_ad.textContent =  `פנימית  `;
+}
+let clear_ae = document.getElementById("ae");
+if(counter__ae === 0){
+clear_ae.textContent =  `סה"כ  `;
+}
+    }
+
+    // function counter_ag(){
+      
+    //   let ag_id =  document.getElementById("ag");
+    //   counter__ag++;
+    //  ag_id.textContent = `ה  = ${counter__ag}`;
+    // }
+    // function counter_ah(){
+      
+    //   let ah_id =  document.getElementById("ah");
+    //   counter__ah++;
+    //  ah_id.textContent = `ה  = ${counter__ah}`;
+    // }
+    // function counter_ai(){
+      
+    //   let ai_id =  document.getElementById("ai");
+    //   counter__ai++;
+    //  ai_id.textContent = `ה  = ${counter__ai}`;
+    // }
+    // function counter_aj(){
+      
+    //   let aj_id =  document.getElementById("aj");
+    //   counter__aj++;
+    //  aj_id.textContent = `ה  = ${counter__aj}`;
+    // }
+    
